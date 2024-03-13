0e419b20c9cf776544c7147a88f0698f38506dcc
Giovanni Maria Ricciolini <151955659+GianmariaRicciolini@users.noreply.github.com>
2024-03-13 15:52:23 +0100

    selected books

--- src/components/SingleBook.jsx
+++ src/components/SingleBook.jsx
@@ -4,13 +4,31 @@ import Card from "react-bootstrap/Card";
 import Col from "react-bootstrap/Col";
 
 class SingleBook extends Component {
+  state = {
+    selected: false,
+  };
+
+  selectedBook = () => {
+    this.setState((prevState) => ({
+      selected: !prevState.selected,
+    }));
+  };
+
   render() {
     const { asin, title, img, price } = this.props;
+    const { selected } = this.state;
 
     return (
       <Col sm={6} lg={4} xl={3} key={asin}>
         <Card className="m-auto" style={{ height: "fit-content", width: "300px" }}>
-          <Card.Img variant="top" src={img} alt={title} style={{ objectFit: "cover", height: "400px" }} />
+          <Card.Img
+            variant="top"
+            src={img}
+            alt={title}
+            style={{ objectFit: "cover", height: "400px" }}
+            className={selected ? "selected-card" : ""} //se la foto è selezionata aggiungi la classe selected-car, altrimenti cancellala
+            onClick={this.selectedBook}
+          />
           <Card.Body className="d-flex flex-column justify-content-around">
             <Card.Title style={{ overflow: "hidden", textOverflow: "ellipsis", whiteSpace: "nowrap" }}>
               {title}
@@ -18,9 +36,9 @@ class SingleBook extends Component {
             <Card.Text>
               <strong>Price:</strong> {price}$
             </Card.Text>
-            <Button className="w-50" variant="success">
+            {/* <Button className="w-50" variant="success">
               Buy Now
-            </Button>
+            </Button> */}
           </Card.Body>
         </Card>
       </Col>
